c90a10ea15117c564b6be7e88c66dc16c6b9a443
Mohammed Al-Eiadeh <93108547+Mohammed-Ryiad-Eiadeh@users.noreply.github.com>
2023-10-15 00:52:03 -0400

    Update DataSet.java

--- Perceptron/src/main/java/DataSet/org/DataSet.java
+++ Perceptron/src/main/java/DataSet/org/DataSet.java
@@ -9,7 +9,7 @@ public class DataSet {
     public HashMap<Integer, Double> getDataSet() {
         HashMap<Integer, Double> dataSet = new HashMap<>();
         for (int x = -10000; x < 10000; x++) {
-            if (x < 0) {
+            if (x <= 0) {
                 dataSet.put(x, 0.0);
             } else {
                 dataSet.put(x, 1.0);
